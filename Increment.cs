0ca49bacea600552611f3a6b890976c7aad496ce
Alicia Guerra <aiguerra@ucsd.edu>
2015-03-31 18:38:37 -0700

    Create Increment.cs

---
+++ Increment.cs
@@ -0,0 +1,17 @@
+using System;
+class Increment
+{
+static void Main(string[] args)
+{
+int c;
+c=5;
+Console.WriteLine(c);
+Console.WriteLine(c++);
+Console.WriteLine(c);
+
+c=5;
+Console.WriteLine(c);
+Console.WriteLine(++c);
+Console.WriteLine(c);
+}//end of method main
+}//end of class increment
