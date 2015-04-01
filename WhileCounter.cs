c8a853a04d3b2952e59e6eeb6b4685b24de74b34
Alicia Guerra <aiguerra@ucsd.edu>
2015-03-31 18:43:25 -0700

    Create WhileCounter.cs

---
+++ WhileCounter.cs
@@ -0,0 +1,13 @@
+using System;
+class WhileCounter
+{
+static void Main(string[] args)
+{
+int counter=1;//initialization
+while(counter<= 5)//repetition condition
+{
+Console.WriteLine(counter);
+counter++;//increment
+}//end while
+}//end method Main
+}//end class WhileCounter
