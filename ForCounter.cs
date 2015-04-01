4fe5d62000d5fb4f4fdbe69e3c5329377fcc92b4
Alicia Guerra <aiguerra@ucsd.edu>
2015-03-31 18:46:27 -0700

    Create ForCounter.cs

---
+++ ForCounter.cs
@@ -0,0 +1,11 @@
+using System;
+class ForCounter
+{
+static void Main(string[] args)
+{
+//initialization, repetition condition and incrementing 
+//are all included in the for structure.
+for(int counter=1; counter<=5; counter++)
+Console.WriteLine(counter);
+}
+}
